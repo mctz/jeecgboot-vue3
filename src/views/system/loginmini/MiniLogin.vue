6791428129462d34bf5fc555d98a0ab56383dc76
msl <987159729@qq.com>
2024-05-23 08:35:56 +0800

    个性化配置

--- src/views/system/loginmini/MiniLogin.vue
+++ src/views/system/loginmini/MiniLogin.vue
@@ -105,7 +105,7 @@
                     <span>{{ t('sys.login.otherSignIn') }}</span>
                   </div>
                 </div>
-                <div class="aui-flex" :class="`${prefixCls}-sign-in-way`">
+                <div class="aui-flex" :class="`${prefixCls}-sign-in-way`" style="display: none">
                   <div class="aui-flex-box">
                     <div class="aui-third-login">
                       <a title="github" @click="onThirdLogin('github')"><GithubFilled /></a>
